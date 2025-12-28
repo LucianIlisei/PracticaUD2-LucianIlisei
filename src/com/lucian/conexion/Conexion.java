8d0ad287524f1fdcf68b1b6f9617bf47b724ba1c
Claudia Espinosa <claudiaespinosa@macbook-air-de-claudia.home>
2025-12-28 20:31:06 +0100

    Métodos creados de conectar, desconectar y leerFichero de la clase conexión. En la clase Modelo.java empezado creando el constructor

--- src/com/lucian/conexion/Conexion.java
+++ src/com/lucian/conexion/Conexion.java
@@ -1,5 +1,9 @@
 package com.lucian.conexion;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -19,7 +23,6 @@ public class Conexion {
         } catch (SQLException e) {
             try {
                 conexion = DriverManager.getConnection("jdbc:mysql://"+ip+"3306",user,password);
-
                 PreparedStatement statement = null;
 
                 String code = leerFichero();
@@ -30,10 +33,31 @@ public class Conexion {
                 }
                 assert statement != null;
                 statement.close();
-            } catch (SQLException ex) {
+            } catch (SQLException | IOException ex) {
                 ex.printStackTrace();
             }
             e.printStackTrace();
+        }
+    }
+
+    private String leerFichero() throws IOException {
+        String linea;
+        StringBuilder stringBuilder = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(new FileReader("basedatos.sql"))) {
+            while((linea = reader.readLine()) != null){
+                stringBuilder.append(linea);
+                stringBuilder.append(" ");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    void desconectar() {
+        try {
+            conexion.close();
+            conexion = null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/com/lucian/gui/Controlador.java
+++ src/com/lucian/gui/Controlador.java
@@ -1,4 +1,18 @@
 package com.lucian.gui;
 
+import com.lucian.conexion.Conexion;
+import com.lucian.utilidades.Utilidades;
+
 public class Controlador {
+    private Modelo modelo;
+    private Conexion conexion;
+    private Vista vista;
+    private Utilidades utilidades;
+
+    public Controlador(Modelo modelo, Conexion conexion, Vista vista, Utilidades utilidades){
+        this.modelo = modelo;
+        this.conexion = conexion;
+        this.vista = vista;
+        this.utilidades = utilidades;
+    }
 }
