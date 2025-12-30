6087c39d7871b1cff3c463fad38f2544a607a099
Lucian Ilisei <lucianioanilisei.trabajos@gmail.com>
2025-12-30 15:39:28 +0100

    Panel paciente creado, panel doctor creado, a falta de crear los últimos 3 paneles de la vista principal.

--- src/com/lucian/gui/Vista.java
+++ src/com/lucian/gui/Vista.java
@@ -1,4 +1,60 @@
 package com.lucian.gui;
 
-public class Vista {
+import com.github.lgooddatepicker.components.DatePicker;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Vista extends JFrame{
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private final static String TITULO_FRAME="Aplicacion hospital";
+
+    // Paciente
+    JPanel PanelMedicamento;
+    JPanel PanelDoctor;
+    JPanel PanelHospital;
+    JPanel PanelCita;
+    JPanel PanelPaciente;
+    JTextField campoNombrePaciente;
+    JTextField campoPrimerApellidoPaciente;
+    JTextField campoSegundoApellidoPaciente;
+    JRadioButton masculinoRadioButtonPaciente;
+    JRadioButton femeninoRadioButtonPaciente;
+    JTextField campoTelefonoPaciente;
+    JTextField campoEmailPaciente;
+    JRadioButton siRadioButtonFumadorPaciente;
+    JRadioButton noRadioButtonFumadorPaciente;
+    JComboBox comboBoxHospitalPaciente;
+    JButton bntAñadirPaciente;
+    JButton btnModificarPaciente;
+    JButton btnEliminarPaciente;
+    private DatePicker fechaNacimientoPacienteDatePicker;
+    private JTable pacientesTabla;
+    private JTextField campoNombreDoctor;
+    private JTextField campoPrimerApellidoDoctor;
+    private JTextField campoSegundoApellidoDoctor;
+    private JTextField campoTelefonoDoctor;
+    private JTextField campoEmailDoctor;
+    private JComboBox comboBoxEspecialidadDoctor;
+    private JComboBox comboBoxHospitalDoctor;
+    private JButton btnAñadirDoctor;
+    private JButton btnModificarDoctor;
+    private JButton btnEliminarDoctor;
+    private DatePicker fechaContratacionDatePickerDoctor;
+    private JTable doctoresTabla;
+
+    public Vista() {
+        super(TITULO_FRAME);
+        initFrame();
+    }
+
+    public void initFrame() {
+        this.setContentPane(panel1);
+        //al clickar en cerrar no hace nada
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
---
+++ src/com/lucian/main/Principal.java
@@ -0,0 +1,11 @@
+package com.lucian.main;
+
+import com.lucian.gui.Vista;
+
+import javax.swing.*;
+
+public class Principal {
+    public static void main(String[] args) {
+        Vista vista = new Vista();
+    }
+}
